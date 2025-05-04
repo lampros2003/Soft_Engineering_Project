29a3b9712e38e0b8c15bccbe4ee58988a5ea9520
Giorgos Konstantouras <68753823+giorgoskon3@users.noreply.github.com>
2025-05-04 14:47:03 +0300

    add navigation from menu screen to main screen

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -2,7 +2,10 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import util.SceneSwitching;
 
 public class MenuController {
 
@@ -10,8 +13,17 @@ public class MenuController {
     private Button testBtn;
 
     @FXML
+    private Button backToMainScreen;
+
+    @FXML
     void btnClicked(ActionEvent event) {
         System.out.println("Test button clicked!");
+    }
+
+    @FXML
+    void nagivateToMainScreen(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        SceneSwitching.switchScene(stage, "/view/MainScreen.fxml");
     }
 
 }
